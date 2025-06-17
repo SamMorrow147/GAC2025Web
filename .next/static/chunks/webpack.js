d4b5aa2313d398e67fdf7740820fda061639f4e5
Sam Morrow <sammorrow@Sams-MacBook-Pro-3.local>
2025-06-17 14:10:12 -0500

    Fix: Remove deprecated next export command

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "418807602a90b07e"; }
+/******/ 		__webpack_require__.h = function() { return "971bc0e64775412b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
